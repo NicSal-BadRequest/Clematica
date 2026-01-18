331a5acb7e52611e39ff75429065b8216919b369
NicSal-BadRequest <salocin-terlosaav@outlook.com>
2026-01-17 21:11:43 -0300

    modificados:     clematicaHPF/modulos/core/clemCore.hs 	modificados:     clematicaHPF/modulos/core/clemCore.hs

--- clematicaHPF/modulos/core/clemCore.hs
+++ clematicaHPF/modulos/core/clemCore.hs
@@ -1,24 +1,38 @@
 module ClemCoreV1
-    (ClemCore(...), 
+    (ClemCore, 
+    Complejo(..),
     sumar,
     producto, 
     division, 
-    ucesor, 
+    sucesor, 
     opuesto)
 where 
 
 -- Para manejar valores complejos
 data Complejo = Lateral Double Double -- Tipo algebraico para los complejos, sería a + bi
-    deriving (Show, Eq) 
+    deriving (Eq) 
 
 data ClemCore = ExpA Int --Defino un Tipo Algebraico que maneje los valores de la calculadora por Pattern Matching
             | ExpB Double
             | ExpC Double Double
             | ExpD Complejo 
             | ExpE Complejo Complejo
-            deriving (Show, Eq)
+            deriving (Eq)
 
--- Operaciones de la interfaz
+-- 1. Instancias SHOW personalizadas
+-- Para Complejo
+instance Show Complejo where
+    show (Lateral a b) = show a ++ " + " ++ show b ++ "i"
+
+-- Para ClemCore
+instance Show ClemCore where
+    show (ExpA x)   = "Valor Entero: " ++ show x
+    show (ExpB x)   = "Valor Real: " ++ show x
+    show (ExpC x y) = "Operación Real: (" ++ show x ++ ", " ++ show y ++ ")"
+    show (ExpD c)   = "Valor Complejo: " ++ show c
+    show (ExpE c1 c2) = "Operación Compleja: (" ++ show c1 ++ ") y (" ++ show c2 ++ ")"
+
+--2. Operaciones de la interfaz
 sucesor :: ClemCore -> ClemCore       -- O(1)
 opuesto :: ClemCore -> ClemCore       -- O(1)
 sumar :: ClemCore -> ClemCore         -- O(1)
@@ -28,42 +42,36 @@ division :: ClemCore -> ClemCore      -- O(1)
 
 -- Definicion de sucesor
 sucesor (ExpA x) = ExpA (x + 1)
-sucesor _ = error "El sucesor esta definido solo para Enteros"
+sucesor _ = error "Error: El sucesor esta definido solo para Enteros."
 
 -- Definicion de opuesto
-opuesto (ExpD (Lateral x y)) = ExpD (Lateral (-1 * x) (-1 * y))
-opuesto (ExpB x) = ExpB (-1 * x)
-opuesto (ExpA x) = ExpA (-1 * x)
+opuesto (ExpD (Lateral x y)) = ExpD (Lateral (-x) (-y))
+opuesto (ExpB x) = ExpB (-x)
+opuesto (ExpA x) = ExpA (-x)
+opuesto _ = error "Error: Opuesto solo no está definido para este constructor."
 
 -- Definicion de sumar
-sumar (ExpE (Lateral x y) (Lateral z w)) = ExpE (Lateral (x + z) (y + w))
-sumar (ExpD (Lateral x y)) = ExpD (Lateral x y)
-sumar (ExpC x y) = ExpC (x + y)
-sumar (ExpB x) = ExpB x
-sumar (ExpA x) = ExpA x
+sumar (ExpE (Lateral x y) (Lateral z w)) = ExpD (Lateral (x + z) (y + w))
+sumar (ExpC x y) = ExpB (x + y)
+sumar _ = error "Error: Constructor incorrecto o falta segundo número."
 
 -- Definicion de restar
-restar (ExpC x y) = ExpC (x - y) 
-restar (ExpB x) = ExpB x
-restar (ExpA x) = ExpA x
+restar (ExpE (Lateral x y) (Lateral z w)) = ExpD (Lateral (x - z) (y - w))
+restar (ExpC x y) = ExpB (x - y) 
+restar _ = error "Error: Constructor incorrecto o falta segundo número."
 
 -- Definicion de producto
-producto (ExpC x y) = ExpC (x * y)
-producto (ExpB x) = ExpB x
-producto (ExpA x) = ExpA x
+-- (x + yi)(z + wi) = (xz - yw) + (xw + yz)i
+producto (ExpE (Lateral x y) (Lateral z w)) = ExpD (Lateral (x*z - y*w) (x*w + y*z))
+producto (ExpC x y) = ExpB (x * y)
+producto _ = error "Error: Constructor incorrecto o falta segundo número."
 
 -- Definicion de division
-division (ExpD _ (Lateral 0 0)) = error "Error: No se puede divir por cero."
 division (ExpC _ 0) = error "Error: No se puede dividir por cero."
-division (ExpC x y) = ExpC (x / y) 
-division (ExpB x) = ExpB x
-division (ExpA x) = ExpB x
-
--- Personalizar la visualización:
-instance Show ClemCore where
-    show (Lateral x y) = "Valor complejo: (" ++ show x ++ ", " ++ show y ++ "i )."
-
-instance Show ClemCore where
-    show (ExpA x)   = "Valor Entero: " ++ show x ++ "."
-    show (ExpB x)   = "Valor Real: " ++ show x ++ "."
-    show (ExpC x y) = "(" ++ show x ++ " , " ++ show y ++ ")."
+division (ExpC x y) = ExpB (x / y) 
+-- Division de complejos multiplicando por el conjugado
+division (ExpE (Lateral x y) (Lateral z w)) 
+    | z == 0 && w == 0 = error "Error: No se puede dividir por cero."
+    | otherwise = let den = z*z + w*w
+                in ExpD (Lateral ((x*z + y*w)/den) ((y*z - x*w)/den))
+division _ = error "Error: Constructor incorrecto o falta segundo número."
